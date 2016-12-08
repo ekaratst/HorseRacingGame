9d413ee8413db8d3f44a1f51146f9f1cf93287c8
ekarat.st <ekaratdev@gmail.com>
2016-12-08 16:34:14 +0700

    ม้าทั้งสองตัวเปลี่ยนรูปได้แล้วแต่มีบัคเรื่องกดปุ่มเดิมซ้ำๆแล้วม้าไปข้างหน้าอยู่

--- horse.py
+++ horse.py
@@ -1,5 +1,5 @@
 import arcade
-from models import World, Horse
+from models import World, HorseA, HorseB
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -25,7 +25,7 @@ class SpaceGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.WHITE)
         self.world = World(width, height)
-        self.backgroung_sprite = ModelSprite('images/background.png', model=self.world.background)
+        self.background_sprite = ModelSprite('images/background.png', model=self.world.background)
         self.horseA1_sprite = ModelSprite('images/racerA1.png', model=self.world.horseA1)
         self.horseB1_sprite = ModelSprite('images/racerB1.png', model=self.world.horseB1)
 
@@ -34,12 +34,38 @@ class SpaceGameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.backgroung_sprite.draw()
-        self.horseA1_sprite.draw()
-        self.horseB1_sprite.draw()
+        self.background_sprite.draw()
+        if self.world.getState_A() == 1:
+            self.getSwitchBegin_A()
+            self.horseA1_sprite.draw()
+        elif self.world.getState_A() == 2:
+            self.getSwitchAfter_A()
+            self.horseA1_sprite.draw()
+        if self.world.getState_B() == 1:
+            self.getSwitchBegin_B()
+            self.horseB1_sprite.draw()
+        elif self.world.getState_B() == 2:
+            self.getSwitchAfter_B()
+            self.horseB1_sprite.draw()
 
     def animate(self, delta):
         self.world.animate(delta)
+
+    def getSwitchAfter_A(self):
+        self.horseA1_sprite = ModelSprite('images/racerA2.png', model=self.world.horseA1)
+        return  self.horseA1_sprite
+
+    def getSwitchBegin_A(self):
+        self.horseA1_sprite = ModelSprite('images/racerA1.png', model=self.world.horseA1)
+        return  self.horseA1_sprite
+
+    def getSwitchAfter_B(self):
+        self.horseB1_sprite = ModelSprite('images/racerB2.png', model=self.world.horseB1)
+        return  self.horseA1_sprite
+
+    def getSwitchBegin_B(self):
+        self.horseB1_sprite = ModelSprite('images/racerB1.png', model=self.world.horseB1)
+        return  self.horseA1_sprite
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,29 +1,50 @@
 import arcade.key
 
-class Horse:
-    DIR_STAYSTILL = 0
-    DIR_MOVE = 1
+class HorseA:
+    DIR_STAYSTILL_A = 0
+    DIR_MOVE_A = 1
 
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
-        self.direction = Horse.DIR_MOVE
+        self.direction_A = HorseA.DIR_MOVE_A
 
-    def switch_direction(self):
-        if self.direction == Horse.DIR_STAYSTILL:
-            self.direction = Horse.DIR_MOVE
+    def switch_direction_A(self):
+        if self.direction_A == HorseA.DIR_STAYSTILL_A:
+            self.direction_A = HorseA.DIR_MOVE_A
         else:
-            self.direction = Horse.DIR_STAYSTILL
+            self.direction_A = HorseA.DIR_STAYSTILL_A
 
     def animate(self, delta):
         if self.x < 630:
-            if self.direction == Horse.DIR_STAYSTILL:
+            if self.direction_A == HorseA.DIR_STAYSTILL_A:
                 self.x += 4
 
-            self.direction = Horse.DIR_MOVE
-            # else:
-            #     self.x += 0
+            self.direction_A = HorseA.DIR_MOVE_A
+
+class HorseB:
+    DIR_STAYSTILL_B = 0
+    DIR_MOVE_B = 1
+
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.direction_B = HorseB.DIR_MOVE_B
+
+    def switch_direction_B(self):
+        if self.direction_B == HorseB.DIR_STAYSTILL_B:
+            self.direction_B = HorseB.DIR_MOVE_B
+        else:
+            self.direction_B = HorseB.DIR_STAYSTILL_B
+
+    def animate(self, delta):
+        if self.x < 630:
+            if self.direction_B == HorseB.DIR_STAYSTILL_B:
+                self.x += 4
+
+            self.direction_B = HorseB.DIR_MOVE_B
 
 class Background:
     def __init__(self, world, x, y):
@@ -36,20 +57,36 @@ class World:
         self.width = width
         self.height = height
 
-        self.horseA1 = Horse(self, 37, 400)
-        self.horseB1 = Horse(self, 20, 180)
+        self.horseA1 = HorseA(self, 37, 400)
+        self.horseB1 = HorseB(self, 20, 180)
+        # self.horseA2 = HorseA(self, 37, 400)
+        # self.horseB2 = HorseB(self, 20, 180)
         self.background = Background(self, 400, 300)
 
     def animate(self, delta):
         self.horseA1.animate(delta)
+        # self.horseA2.animate(delta)
         self.horseB1.animate(delta)
+        # self.horseB2.animate(delta)
 
+    state_a = 1
+    state_b = 1
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.RIGHT:
-            self.horseA1.switch_direction()
+            self.horseA1.switch_direction_A()
+            self.state_a = 1
         elif key == arcade.key.LEFT:
-            self.horseA1.switch_direction()
+            self.horseA1.switch_direction_A()
+            self.state_a = 2
         if key == arcade.key.D:
-            self.horseB1.switch_direction()
+            self.horseB1.switch_direction_B()
+            self.state_b = 1
         elif key == arcade.key.A:
-            self.horseB1.switch_direction()
+            self.horseB1.switch_direction_B()
+            self.state_b = 2
+
+    def getState_A(self):
+        return self.state_a
+
+    def getState_B(self):
+        return self.state_b
