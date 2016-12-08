de721335f79dec1448d1920efaf344bab4dc2f4e
ekarat.st <ekaratdev@gmail.com>
2016-12-08 11:54:44 +0700

    เพิ่มModelSprite และแยกBackgroundให้เหมือนHorse

--- horse.py
+++ horse.py
@@ -5,30 +5,39 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.WHITE)
-        self.backgroung = arcade.Sprite('images/background.png')
-        self.backgroung.set_position(400, 300)
-
-        self.horseA1_sprite = arcade.Sprite('images/racerA1.png')
-        self.horseB1_sprite = arcade.Sprite('images/racerB1.png')
         self.world = World(width, height)
+        self.backgroung_sprite = ModelSprite('images/background.png', model=self.world.background)
+        self.horseA1_sprite = ModelSprite('images/racerA1.png', model=self.world.horseA1)
+        self.horseB1_sprite = ModelSprite('images/racerB1.png', model=self.world.horseB1)
+
 
     def on_draw(self):
         arcade.start_render()
-        self.backgroung.draw()
+        self.backgroung_sprite.draw()
         self.horseA1_sprite.draw()
         self.horseB1_sprite.draw()
 
     def animate(self, delta):
         self.world.animate(delta)
-        self.backgroung.set_position(self.backgroung.center_x, self.backgroung.center_y)
-        self.horseA1_sprite.set_position(self.world.horseA1.x, self.world.horseA1.y)
-        self.horseB1_sprite.set_position(self.world.horseB1.x, self.world.horseB1.y)
-
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -8,6 +8,12 @@ class Horse:
         if self.x < 630:
             self.x += 4
 
+class Background:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
 class World:
     def __init__(self, width, height):
         self.width = width
@@ -15,6 +21,7 @@ class World:
 
         self.horseA1 = Horse(self, 37, 400)
         self.horseB1 = Horse(self, 20, 180)
+        self.background = Background(self, 400, 300)
 
     def animate(self, delta):
         self.horseA1.animate(delta)
